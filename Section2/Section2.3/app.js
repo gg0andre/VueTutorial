ed208a8be44b9e145730bac9c3df8cfe1243147b
Andre Tran <andreviet2009@hotmail.com>
2023-02-22 16:40:48 -0500

    40

--- Section2/Section2.3/app.js
+++ Section2/Section2.3/app.js
@@ -3,14 +3,31 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      // fullname: "",
     };
+  },
+  watch: {
+    counter(value){
+      if(value > 50) {
+        this.counter = 0
+      }
+    }
+    // name(value) {
+    //   if (value === "") this.fullname = "";
+    //   else this.fullname = value + " " + this.lastName;
+    // },
+    // lastName(value) {
+    //   if (value === "") this.fullname = "";
+    //   else this.fullname = this.name + " " + value;
+    // }
   },
   computed: {
     fullname() {
-      if (this.name === "") {
+      if (this.name === "" || this.lastName === "") {
         return "";
       }
-      return this.name + " " + "Tran";
+      return this.name + " " + this.lastName;
     },
   },
   methods: {
