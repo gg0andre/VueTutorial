a170fc60e5006de89970acf9ae59aa7fabb06397
Andre Tran <andreviet2009@hotmail.com>
2023-02-27 11:23:57 -0500

    70

---
+++ Section5/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+    },
+  },
+});
+
+app.mount("#app");
+
+const app2 = Vue.createApp({
+  template: `<p>{{ favoriteMeal}}`,
+  data() {
+    return {
+      favoriteMeal: "Pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
+
