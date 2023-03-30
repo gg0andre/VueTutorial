fa5f612441c9b70d0184240d45a1d674ebb056b4
Andre Tran <andreviet2009@hotmail.com>
2023-03-30 01:33:14 -0400

    8 and 8.5

---
+++ Assignment6/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <form @submit.prevent="SubmitData">
+      <input type="text" placeholder="Your name" v-model="enteredName" />
+      <input type="text" placeholder="Your age" v-model="enteredAge" />
+      <button>Set User Data</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["set-data"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    SubmitData() {
+      this.$emit("set-data", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
